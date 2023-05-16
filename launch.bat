ee3c44be3dd3a0bd21a72daf16eb033f585ac8ee
ItsLynix <lynixthewolf@gmail.com>
2023-05-16 03:34:13 +0200

    powershell: set execution-policy again

--- launch.bat
+++ launch.bat
@@ -1,5 +1,5 @@
 @echo off
 echo "Attempting to set PowerShell execution policy..."
-Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
+powershell Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
 echo "Launching manager..."
 powershell.exe .\manager.ps1
